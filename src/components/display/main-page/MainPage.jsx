c31ad0b94f6e395e443508d8c84b22633fbc1464
Na3ar-17 <gavruk2017@gmail.com>
2024-08-06 20:59:11 +0200

    implemented icons position

--- src/components/display/main-page/MainPage.jsx
+++ src/components/display/main-page/MainPage.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../../common/navbar/NavBar'
 import Button from '../../ui/button/Button'
 import styles from './MainPage.module.scss'
+import { icons } from './icons'
 const MainPage = () => {
 	return (
 		<section className={styles.container}>
@@ -18,6 +19,25 @@ const MainPage = () => {
 					</div>
 				</div>
 			</div>
+			<img className={styles.bitcoin} src={icons.bitcoin} alt={icons.bitcoin} />
+			<img
+				className={styles.litecoin}
+				src={icons.litecoin}
+				alt={icons.litecoin}
+			/>
+			<img className={styles.monero} src={icons.monero} alt={icons.monero} />
+			<img
+				className={styles.etherium}
+				src={icons.etherium}
+				alt={icons.etherium}
+			/>
+			<img
+				className={styles.starMini}
+				src={icons.starMini}
+				alt={icons.starMini}
+			/>
+			<img className={styles.star} src={icons.star} alt={icons.star} />
+			<img className={styles.star} src={icons.star} alt={icons.star} />
 		</section>
 	)
 }
---
+++ src/components/display/main-page/icons.js
@@ -0,0 +1,8 @@
+import bitcoin from '/public/icons/Bitcoin-1.png'
+import etherium from '/public/icons/Etherium-2.png'
+import litecoin from '/public/icons/LiteCoin-1.png'
+import monero from '/public/icons/Monero-2.png'
+import starMini from '/public/icons/star-img-mini.png'
+import star from '/public/icons/star-img.png'
+
+export const icons = { bitcoin, etherium, litecoin, monero, star, starMini }
