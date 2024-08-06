7a56fc7645e06c6f4314d56a7bbde078b89124b9
Na3ar-17 <gavruk2017@gmail.com>
2024-08-06 10:51:38 +0200

    installed font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-	return <></>
+	return (
+		<>
+			<p className='text-xl'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+				ratione explicabo dolor corrupti voluptatem vitae rerum nesciunt placeat
+				fuga aut, saepe repellat, cupiditate quae fugiat eum quia nihil commodi
+				nulla.
+			</p>
+		</>
+	)
 }
 
 export default App
