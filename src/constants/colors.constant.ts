42d01cf0664ab5c913e484ce55e71d19641d4312
Na3ar-17 <gavruk2017@gmail.com>
2024-08-06 10:43:34 +0200

    created colors constant

---
+++ src/constants/colors.constant.ts
@@ -0,0 +1,22 @@
+export const COLORS = {
+	primary: '#b982ff',
+	bg: '#0b0b0f',
+
+	gray: {
+		100: '#212529',
+		80: '#495057',
+		60: '#6e7584',
+		50: '#adb5bd',
+		40: '#ced4da',
+		30: '#e9ecef',
+		20: '#f1f3f5',
+		10: '#f8f9fa',
+	},
+
+	system: {
+		error: '#991d21',
+		info: '#4f75ff',
+		warning: '#ff9f4c',
+		success: '#7ec136',
+	},
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import { COLORS } from './src/constants/colors.constant'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		colors: COLORS,
 	},
 	plugins: [],
 }
