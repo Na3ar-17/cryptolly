4a509a6284708195aa58f77f095510d7a4ec3fea
Na3ar-17 <gavruk2017@gmail.com>
2024-08-06 20:15:53 +0200

    created text block

--- src/components/display/main-page/MainPage.jsx
+++ src/components/display/main-page/MainPage.jsx
@@ -1,10 +1,24 @@
 import NavBar from '../../common/navbar/NavBar'
+import Button from '../../ui/button/Button'
 import styles from './MainPage.module.scss'
 const MainPage = () => {
 	return (
-		<main>
+		<section className={styles.container}>
 			<NavBar />
-		</main>
+			<div className={styles.content}>
+				<div className={styles['text-block']}>
+					<h1 className={styles.heading}>Own Your Crypto Adventure</h1>
+					<p className={styles.text}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi{' '}
+					</p>
+					<div>
+						<Button size='lg' label={'Get Started'} />
+					</div>
+				</div>
+			</div>
+		</section>
 	)
 }
 
