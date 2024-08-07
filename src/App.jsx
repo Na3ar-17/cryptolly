ef070863984df0fc6352916ea10624650ab22f47
Na3ar-17 <gavruk2017@gmail.com>
2024-08-07 08:52:37 +0200

    started working on about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import AboutUs from './components/display/about-us/AboutUs'
 import MainPage from './components/display/main-page/MainPage'
 
 function App() {
 	return (
 		<>
 			<MainPage />
+			{/* <AboutUs /> */}
 		</>
 	)
 }
---
+++ src/components/display/about-us/AboutUs.jsx
@@ -0,0 +1,10 @@
+import styles from './AboutUs.module.scss'
+const AboutUs = () => {
+	return (
+		<section className={styles.container}>
+			{/* <div className={styles.banner}></div> */}
+		</section>
+	)
+}
+
+export default AboutUs
