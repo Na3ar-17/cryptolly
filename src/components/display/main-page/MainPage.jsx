bca7c06a1d3c11d541bfe8e032b4f9b77ccdde80
Na3ar-17 <gavruk2017@gmail.com>
2024-08-08 16:46:14 +0200

    finished main page

--- src/components/display/main-page/MainPage.jsx
+++ src/components/display/main-page/MainPage.jsx
@@ -1,5 +1,6 @@
 import NavBar from '../../common/navbar/NavBar'
 import Button from '../../ui/button/Button'
+import { icons } from './icons'
 import styles from './MainPage.module.scss'
 const MainPage = () => {
 	return (
@@ -25,17 +26,35 @@ const MainPage = () => {
 				</div>
 			</div>
 			{/* <img src='/public/images/Bg.png' alt='bg' className={styles.bg} /> */}
-			{/* <img className={styles.bg2} src='/public/images/Bg2.png' alt='bg-2' /> */}
 			<div className={styles.circle1}></div>
 			<div className={styles.circle2}></div>
 			<div className={styles.circle3}></div>
-
 			<div className={styles.gradient1}></div>
 			<div className={styles.gradient2}></div>
 			<div className={styles.gradient3}></div>
 			<div className={styles.gradient4}></div>
 			<div className={styles.gradient5}></div>
 			<div className={styles.gradient6}></div>
+
+			<img src={icons.bitcoin} alt={icons.bitcoin} className={styles.bitcoin} />
+			<img
+				src={icons.litecoin}
+				alt={icons.litecoin}
+				className={styles.litecoin}
+			/>
+			<img src={icons.monero} alt={icons.monero} className={styles.monero} />
+			<img
+				src={icons.etherium}
+				alt={icons.etherium}
+				className={styles.etherium}
+			/>
+			<img src={icons.star} alt={icons.star} className={styles.star} />
+			<img src={icons.star} alt={icons.star} className={styles.star2} />
+			<img
+				src={icons.starMini}
+				alt={icons.starMini}
+				className={styles.starMini}
+			/>
 		</section>
 	)
 }
